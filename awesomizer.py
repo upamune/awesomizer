48398c79bf7aec9ef9e82c882ae7feb56da3d601
upamune <jajkeqos@gmail.com>
2015-09-28 21:33:50 +0900

    生成する画像の拡張子がpng 固定になっていたのを修正した

--- awesomizer.py
+++ awesomizer.py
@@ -27,24 +27,31 @@ def get_image_file_name():
     return sys.argv[1]
 
 image_file_name = get_image_file_name()
-name = os.path.basename(image_file_name)
-save_file_name = "./awesomized_%s" % (name)
-image = Image.open(image_file_name)
-text = u"最高"
+file_name = os.path.basename(image_file_name)
+save_file_name = "./awesomized_%s" % (file_name)
 
+image = Image.open(image_file_name)
+
+text = u"最高"
+font_color = (255, 255, 255)
 font_file = "./fonts/YasashisaAntique.otf"
 font_size = calc_font_size(image.size)
 font_pos = (font_size * len(text), font_size)
 font = ImageFont.truetype(font_file, font_size, encoding='utf-8')
-awesome = Image.new('RGBA', font_pos, (255, 255, 255, 0))
-shadow_pos = (int(font_size * 0.008), int(font_size * 0.008))
-draw = ImageDraw.Draw(awesome)
-draw.text(shadow_pos, text, fill=(170, 191, 217), font=font)
-draw.text((0, 0), text, fill="white", font=font)
 
+awesome = Image.new('RGBA', font_pos, (255, 255, 255, 0))
 alpha_filter = Image.new('RGBA', image.size, (255, 255, 255, 100))
+
+shadow_color = (170, 191, 217)
+shadow_pos = (int(font_size * 0.008), int(font_size * 0.008))
 paste_pos = calc_paste_pos(image.size, awesome.size)
+
+draw = ImageDraw.Draw(awesome)
+draw.text(shadow_pos, text, fill=shadow_color, font=font)
+draw.text((0, 0), text, fill=font_color, font=font)
+
 
 image.paste(alpha_filter, (0, 0), alpha_filter)
 image.paste(awesome, paste_pos, awesome)
+
 image.save(save_file_name)
